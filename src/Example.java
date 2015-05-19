ef8bc0113a3cdc868806cc391082229354536435
qq523407234 <523407234@qq.com>
2015-05-19 22:59:40 +0800

    连接mysql

---
+++ src/Example.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class Example {
+
+	public static void main(String[] args) {
+		String driver = "sun.jdbc.odbc.JdbcOdbcDriver";
+		// String URL = "jdbc:odbc:Students";
+		String URL = "jdbc:odbc={Microsoft Access Driver (*.mdb,*.accdb)};DBQ=Students.mdb";
+		Connection con = null;
+		try {
+			Class.forName(driver);
+		} catch (java.lang.ClassNotFoundException e) {
+			System.out.println("Can't load Driver");
+		}
+		try {
+			con = DriverManager.getConnection(URL);
+			System.out.println("Connect Successful.");
+		} catch (SQLException ex) {
+			System.out.println("Connect fail:" + ex.getMessage());
+		}
+	}
+}
